8d146e248141cf01b0b4099c76a339aef266073f
CyrilBaah <cyril.baah@amalitech.org>
2023-03-31 14:46:07 +0000

    generate fakers

---
+++ exercises/management/commands/seed_exercises.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+from exercises.models import Exercises
+
+class Command(BaseCommand):
+    help = 'Seeds the Exercise table with fake data'
+    
+    # def handle(self, *args, **kwargs):
+    #     print("hello")
+
+    def handle(self, *args, **options):
+        fake = Faker()
+
+        # create 100 exercises with fake data
+        for i in range(100):
+            exercise = Exercises(
+                name=fake.word().capitalize(),
+                type=fake.random_element(elements=('cardio', 'strength', 'flexibility')),
+                description=fake.sentence(),
+                user_id=fake.random_int(min=1, max=3) # Assuming 10 users with IDs 1-10
+            )
+            exercise.save()
+
+        self.stdout.write(self.style.SUCCESS('Successfully seeded Exercise table'))
