7ca7dbd8360e1ada5a5f7a03fb74e627ac0550a5
CyrilBaah <cyril.baah@amalitech.org>
2023-04-02 11:54:14 +0000

    refactor seeders

--- exercises/management/commands/seed_exercises.py
+++ exercises/management/commands/seed_exercises.py
@@ -1,12 +1,11 @@
 from django.core.management.base import BaseCommand
 from faker import Faker
+
 from exercises.models import Exercises
 
+
 class Command(BaseCommand):
-    help = 'Seeds the Exercise table with fake data'
-    
-    # def handle(self, *args, **kwargs):
-    #     print("hello")
+    help = "Seeds the Exercise table with fake data"
 
     def handle(self, *args, **options):
         fake = Faker()
@@ -15,10 +14,10 @@ class Command(BaseCommand):
         for i in range(100):
             exercise = Exercises(
                 name=fake.word().capitalize(),
-                type=fake.random_element(elements=('cardio', 'strength', 'flexibility')),
+                type=fake.random_element(elements=("cardio", "strength", "flexibility")),
                 description=fake.sentence(),
-                user_id=fake.random_int(min=1, max=3) # Assuming 10 users with IDs 1-10
+                user_id=fake.random_int(min=1, max=3),  # Assuming 10 users with IDs 1-10
             )
             exercise.save()
 
-        self.stdout.write(self.style.SUCCESS('Successfully seeded Exercise table'))
+        self.stdout.write(self.style.SUCCESS("Successfully seeded Exercise table"))
